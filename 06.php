bb748d62f6b109e9cc38bb42e8f1281c2260971d
Akbar T. Ramadan <113463292+barrrliebert@users.noreply.github.com>
2023-02-15 10:25:19 +0700

    Add files via upload

---
+++ 06.php
@@ -0,0 +1,16 @@
+<html>
+    <body>
+        <title>Writing PHP Function which returns value</title>
+</head>
+<body>
+    <?php
+    function addFunction($num1, $num2)
+    {
+         $sum = $num1 + $num2;
+         return $sum;  
+    }
+    $return_value = addFunction(10,20);
+    echo "Returned value from the function : $return_value";
+    ?>
+    </body>
+    </html>
